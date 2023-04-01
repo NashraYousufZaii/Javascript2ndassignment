9f45a4b660e71b6ab45c993dd38070269c9aa89e
NashraYousufZaii <123868324+NashraYousufZaii@users.noreply.github.com>
2023-04-02 00:13:54 +0500

    Add files via upload

---
+++ jss/index.js
@@ -0,0 +1,41 @@
+alert("I am learning javascript");
+alert("Welcome to JS land  \nHappy Coding!");
+alert("Hello...I can run JS through my web brower's console");
+var myname = "My name is Nashra";
+alert(myname);
+var age = "i am 17";
+alert(age);
+alert("PIZZA \nPIZZ \nPIZ \nPI \nP");
+var bookname =
+  " I am trying to learn from the Book A smarter way to learn JavaScript";
+alert(bookname);
+var html = "Yah!!! I can write HTML content through Javascript";
+document.write(html);
+document.write("<br>");
+
+alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+
+alert("sum of 2+2 =" + (2 + 2));
+var Num = 3;
+var numm = 5;
+var totalnum = Num + numm;
+console.log("sum of " + Num + "and" + numm + " is " + totalnum);
+document.write("<br>");
+
+var num;
+document.write("value after variable declaration is " + num);
+document.write("<br>");
+num = 5;
+document.write("Initial value:" + num);
+document.write("<br>");
+num = num + 1;
+document.write("Vawordlue after increment is:" + num);
+document.write("<br>");
+num = num + 7;
+document.write("Value after addition is:" + num);
+document.write("<br>");
+num = num - 1;
+document.write("Value after Decerment is:" + num);
+document.write("<br>");
+num = num % num;
+document.write("The Reminder is:" + num);
